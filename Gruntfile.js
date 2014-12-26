6637ab40890452b6042385fd48c8613e16bb0166
Vincent Garnier <forxer@gmail.com>
2014-12-26 12:14:53 +0100

    playing with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,54 +5,79 @@ module.exports = function(grunt) {
 
         project: grunt.file.readJSON('package.json'),
 
-        concat: {
-            options: {
-                separator: ';',
-            },
-            dist: {
-                src: [
-                    './bower_components/jquery/jquery.js',
-                    './bower_components/bootstrap/dist/js/bootstrap.js',
-                    './app/assets/javascript/frontend.js'
-                ],
-                dest: './public/assets/javascript/frontend.js',
-            },
-        },
+        projectBanner:
+            '/*!\n'
+            + ' * <%= project.name %> <%= project.version %>\n'
+            + ' * File generated on <%= grunt.template.today("yyyy-mm-dd") %>\n'
+            + ' */\n',
+
         less: {
             development: {
                 options: {
-                  compress: false,
-                  banner:
-                        '/*!\n'
-                      + ' * <%= project.name %> <%= project.version %>\n'
-                      + ' * File generated on <%= grunt.template.today("yyyy-mm-dd") %>\n'
-                      + ' */\n'
+                  compress: false
                 },
                 files: {
                   "./Assets/css/main.css": "./Assets/less/main.less"
                 }
             }
         },
+
+        concat: {
+            options: {
+                separator: ';',
+            },
+            js: {
+                src: [
+                    './Components/jquery/dist/jquery.js',
+                    './Components/bootstrap/dist/js/bootstrap.js',
+                    './Components/select2/select2.js',
+                    './Assets/js/main.js'
+                ],
+                dest: './Assets/prod/app.js',
+            },
+            css: {
+                options: {
+                    banner: '<%= projectBanner%>'
+                },
+                src: [
+           //         './Components/bootstrap/dist/css/bootstrap.min.css',
+           //         './Components/bootstrap/dist/css/bootstrap-theme.min.css',
+                    './Components/fontawesome/css/font-awesome.css',
+                    './Components/select2/select2.css',
+                    './Components/select2/select2-bootstrap.css',
+                    './Assets/css/main.css'
+                ],
+                dest: './Assets/prod/app.css'
+            }
+        },
+
         uglify: {
-          //...
+            options: {
+                banner: '<%= projectBanner%>'
+            },
+            dist: {
+                files: {
+                    './Assets/prod/app.js': './Assets/prod/app.js'
+                }
+            }
         },
-        phpunit: {
-          //...
-        },
-        watch: {
-          //...
+
+        cssmin: {
+            dist: {
+                files: {
+                    './Assets/prod/app.css': './Assets/prod/app.css'
+                }
+            }
         }
     });
 
     // Plugin loading
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
- //   grunt.loadNpmTasks('grunt-phpunit');
- //   grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Task definition
-    grunt.registerTask('init', ['copy', 'less', 'concat', 'uglify']);
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['less', 'concat', 'uglify', 'cssmin']);
+
 };
